ed493be1b1f06a768782d94d50745f1baa606569
varshalj <79315240+varshalj@users.noreply.github.com>
2026-03-03 23:06:42 +0530

    Update create-supabase-server.ts

--- lib/server/create-supabase-server.ts
+++ lib/server/create-supabase-server.ts
@@ -1,18 +1,6 @@
-import { createServerClient } from "@supabase/auth-helpers-nextjs"
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 
 export function createSupabaseFromRequest() {
-  const cookieStore = cookies()
-
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-      },
-    }
-  )
+  return createRouteHandlerClient({ cookies })
 }
